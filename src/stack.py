002f573513a41bc98d3ea7d42181298bc34b817d
Sergey Shiryaev <shiryaev_fox@mail.ru>
2023-12-30 01:44:09 +0300

    HW2 init. Add method pop at Stack class. Add tests for method pop.

--- src/stack.py
+++ src/stack.py
@@ -34,4 +34,9 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+            return None
+        else:
+            popped_node = self.top
+            self.top = self.top.next_node
+            return popped_node.data
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -15,3 +15,22 @@ class TestPush(unittest.TestCase):
         self.assertEqual(stack.top.next_node.next_node.next_node, None)
         with self.assertRaises(AttributeError):
             self.assertEqual(stack.top.next_node.next_node.next_node.data, AttributeError)
+
+
+class TestPopThirst(unittest.TestCase):
+    def test_pop_thirst(self):
+        stack = Stack()
+        stack.push('node1')
+        data = stack.pop()
+        self.assertIsNone(stack.top)
+        self.assertEqual(data, 'node1')
+
+
+class TestPopSecond(unittest.TestCase):
+    def test_pop_second(self):
+        stack = Stack()
+        stack.push('node1')
+        stack.push('node2')
+        data = stack.pop()
+        self.assertEqual(stack.top.data, 'node1')
+        self.assertEqual(data, 'node2')
